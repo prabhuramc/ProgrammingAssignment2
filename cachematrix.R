5af30c3b9d44caf07d94917a369142544d71389e
prabhuramc <prabhuram.chelladurai@gmail.com>
2016-04-08 17:51:20 -0500

    Changes were done on Apr 8th 2016 to cachematrix.R and posted to github repository by Prabhuram Chelladurai as part of R programming Assignment 2 in Coursera.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions were written by prabhuram.chelladurai@gmail.com as part of Assignment 2 in Coursera R programming language course. 
+## There are two functions as below, where makeCacheMatrix() creates a copy or caches a inverse of a matrix. The function cacheSolve, calls the inverse function solve, if it finds no cache. 
 
-## Write a short comment describing this function
+## The function makeCacheMatrix() creates a copy or caches a inverse of a matrix. 
 
 makeCacheMatrix <- function(x = matrix()) {
+
+  m <- NULL
+  set <- function(y) {
+          x <<- y
+          m <<- NULL
+  }
+  get <- function() x
+  setcachemat <- function(solve) m <<- solve
+  getcachemat <- function() m
+  list(set = set, get = get,
+       setcachemat = setcachemat,
+       getcachemat = getcachemat)
+
+
 
 }
 
 
-## Write a short comment describing this function
+## The function cacheSolve, calls the inverse function solve, if it finds no cache. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+        m <- x$getcachemat()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
+
+
 }
